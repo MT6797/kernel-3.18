0177a3644b443c4a4154d52e4b9c5698fadcf7a0
aka <aka.jiang@hotmail.com>
2016-05-25 19:14:33 +0800

    [kernel] [camera] [imx219 mirror]
    Change-Id: I0de34efbff8dc129d78fd31e2a2a8df7ec7ed625

--- drivers/misc/mediatek/imgsensor/src/mt6797/imx219_mipi_raw/imx219mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt6797/imx219_mipi_raw/imx219mipiraw_Sensor.c
@@ -140,7 +140,7 @@ static imgsensor_info_struct imgsensor_info = {
 	.sensor_interface_type = SENSOR_INTERFACE_TYPE_MIPI,//sensor_interface_type
 	.mipi_sensor_type = MIPI_OPHY_NCSI2, //0,MIPI_OPHY_NCSI2;  1,MIPI_OPHY_CSI2
 	.mipi_settle_delay_mode = 1,//0,MIPI_SETTLEDELAY_AUTO; 1,MIPI_SETTLEDELAY_MANNUAL
-	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_R,//sensor output first pixel color
+	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_B,//sensor output first pixel color
 	.mclk = 24,//mclk value, suggest 24 or 26 for 24Mhz or 26Mhz
 	.mipi_lane_num = SENSOR_MIPI_4_LANE,//mipi lane num
 	.i2c_addr_table = {0x21, 0x20, 0xff},//record sensor support all write id addr, only supprt 4must end with 0xff
@@ -528,6 +528,7 @@ static void night_mode(kal_bool enable)
 static void sensor_init(void)
 {
 	LOG_INF("E\n");
+	write_cmos_sensor(0x0172, 0x03);
 	//write_cmos_sensor(0x0100,0x01);  //wake up
 }	/*	sensor_init  */
 
