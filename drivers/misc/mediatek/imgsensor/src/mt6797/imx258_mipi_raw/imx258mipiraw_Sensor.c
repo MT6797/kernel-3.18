006d00c52db6ef35b7756d4662f5a4aa945db043
wenhao.deng <wenhao.deng@new-bund.net>
2016-07-01 14:49:27 +0800

    [k15tb_a] camera: add main camera imx258 driver
    Change-Id: I743af7fe355a65cc8aab591d7be64e80d7a6f3c3

--- drivers/misc/mediatek/imgsensor/src/mt6797/imx258_mipi_raw/imx258mipiraw_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt6797/imx258_mipi_raw/imx258mipiraw_Sensor.c
@@ -169,7 +169,7 @@ static imgsensor_info_struct imgsensor_info = {
 	.sensor_interface_type = SENSOR_INTERFACE_TYPE_MIPI,//sensor_interface_type
     .mipi_sensor_type = MIPI_OPHY_NCSI2, //0,MIPI_OPHY_NCSI2;  1,MIPI_OPHY_CSI2
     .mipi_settle_delay_mode = MIPI_SETTLEDELAY_MANUAL,//0,MIPI_SETTLEDELAY_AUTO; 1,MIPI_SETTLEDELAY_MANNUAL
-	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_B, //SENSOR_OUTPUT_FORMAT_RAW_Gr,//SENSOR_OUTPUT_FORMAT_RAW_R,//sensor output first pixel color
+	.sensor_output_dataformat = SENSOR_OUTPUT_FORMAT_RAW_R,//SENSOR_OUTPUT_FORMAT_RAW_B, //SENSOR_OUTPUT_FORMAT_RAW_Gr,//SENSOR_OUTPUT_FORMAT_RAW_R,//sensor output first pixel color
 	.mclk = 24,//mclk value, suggest 24 or 26 for 24Mhz or 26Mhz
 	.mipi_lane_num = SENSOR_MIPI_4_LANE,//mipi lane num
 	.i2c_addr_table = {0x34, 0x20, 0xff},//record sensor support all write id addr, only supprt 4must end with 0xff
@@ -1315,7 +1315,7 @@ static void sensor_init(void)
 
 	imx258_ImageQuality_Setting();
 	/*Need Mirror/Flip*/
-	set_mirror_flip(3);
+	set_mirror_flip(0);
 
 	load_imx258_SPC_Data();
 	write_cmos_sensor(0x7BC8,0x01);
